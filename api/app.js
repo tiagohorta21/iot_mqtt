72d45e2d675ee7d5207b9aebcd5e3b12a3bd974d
tiagohorta21 <tiago_horta95@hotmail.com>
2021-06-11 15:19:12 +0100

    main: add all functionality

--- api/app.js
+++ api/app.js
@@ -1,13 +1,61 @@
 const express = require('express')
 const app = express()
 const port = 3000
+let startTimer = false;
+let timer = 0;
+
+function updateTimer() {
+  if(startTimer) {
+    timer+= 1000;
+  }
+}
+
+function millisToMinutesAndSeconds(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return (minutes < 10 ? '0' : '') + minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
 
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  startTimer = true;
+  const voltage = 1;
+  const maxVoltageTimer = 0;
+
+  res.render('index', { 
+     voltage:voltage, 
+     timer: millisToMinutesAndSeconds(timer),
+     maxVoltageTimer: maxVoltageTimer
+  })
+})
+
+app.get('/values', (req, res) => {
+  const voltage = 1;
+  const maxVoltageTimer = 0;
+
+  console.log(millisToMinutesAndSeconds(timer), timer)
+
+  res.send({
+    voltage,
+    maxVoltageTimer,
+    timer: millisToMinutesAndSeconds(timer),
+  })
+})
+
+app.get('/on', (req, res) => {
+  startTimer = true;
+  res.status(200).send()
+})
+
+app.get('/off', (req, res) => {
+  startTimer = false;
+  timer = 0;
+
+  res.status(200).send()
 })
 
 app.listen(port, () => {
+  setInterval(updateTimer, 1000);
   console.log(`Example app listening at http://localhost:${port}`)
 })
