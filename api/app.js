c32d1a42aafc2583afc79e7bed0b1cda67e94bfb
tiagohorta21 <tiago_horta95@hotmail.com>
2021-06-11 15:56:56 +0100

    main: Remove max voltage

--- api/app.js
+++ api/app.js
@@ -1,8 +1,18 @@
+// Modules
 const express = require('express')
+const mqtt = require('mqtt')
 const app = express()
+// MQTT
+const client  = mqtt.connect('mqtt://192.168.1.202')
 const port = 3000
+
+app.set('view engine', 'pug')
+const TOPIC_COMMANDS = 'commands'
+const TOPIC_VALUES = 'values'
+
 let startTimer = false;
 let timer = 0;
+const messages = [];
 
 function updateTimer() {
   if(startTimer) {
@@ -11,44 +21,38 @@ function updateTimer() {
 }
 
 function millisToMinutesAndSeconds(millis) {
-  var minutes = Math.floor(millis / 60000);
-  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  let minutes = Math.floor(millis / 60000);
+  let seconds = ((millis % 60000) / 1000).toFixed(0);
   return (minutes < 10 ? '0' : '') + minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
-
-app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
   startTimer = true;
   const voltage = 1;
-  const maxVoltageTimer = 0;
 
   res.render('index', { 
      voltage:voltage, 
-     timer: millisToMinutesAndSeconds(timer),
-     maxVoltageTimer: maxVoltageTimer
+     timer: millisToMinutesAndSeconds(timer)
   })
 })
 
 app.get('/values', (req, res) => {
   const voltage = 1;
-  const maxVoltageTimer = 0;
-
-  console.log(millisToMinutesAndSeconds(timer), timer)
 
   res.send({
     voltage,
-    maxVoltageTimer,
-    timer: millisToMinutesAndSeconds(timer),
+    timer: millisToMinutesAndSeconds(timer)
   })
 })
 
 app.get('/on', (req, res) => {
+  client.publish(TOPIC_COMMANDS, 'on')
   startTimer = true;
   res.status(200).send()
 })
 
 app.get('/off', (req, res) => {
+  client.publish(TOPIC_COMMANDS, 'off')
   startTimer = false;
   timer = 0;
 
@@ -56,6 +60,36 @@ app.get('/off', (req, res) => {
 })
 
 app.listen(port, () => {
+  client.on('connect', function (response) {
+    if(response.cmd === 'connack') {
+      console.info('MQTT: Connected sucessfully')
+    } else {
+      console.error('MQTT: Error', response)
+    }
+
+    client.subscribe(TOPIC_VALUES, function (err) {
+      if (err) {
+        console.error(err)
+      }
+    })
+
+    client.subscribe(TOPIC_COMMANDS, function (err) {
+      if (err) {
+        console.error(err)
+      }
+    })
+  })
+   
+  client.on('message', function (topic, message) {
+    switch (topic) {
+      case TOPIC_VALUES:
+        messages.push(message.toString())
+        break;
+      default:
+        console.log(message.toString())
+        break;
+    }
+  })
   setInterval(updateTimer, 1000);
   console.log(`Example app listening at http://localhost:${port}`)
 })
