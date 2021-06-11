89e24936382e1a821c7a2b6b099888ff41c29bf8
tiagohorta21 <tiago_horta95@hotmail.com>
2021-06-11 13:31:58 +0100

    main: Add pug templat

--- api/app.js
+++ api/app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
 app.listen(port, () => {
